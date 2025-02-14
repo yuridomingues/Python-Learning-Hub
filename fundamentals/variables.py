59d55eb6e2cdf01dd777b945023a20063523f3b1
yuridomingues <yuridomingues.contato@gmail.com>
2025-02-14 15:38:03 -0300

    feat: add variable explained

--- fundamentals/variables.py
+++ fundamentals/variables.py
@@ -7,3 +7,26 @@ radius = 5
 radius_squared = radius ** 2
 
 print(pi * radius_squared)
+
+# You can only use letters, numbers and underscore to define variable names
+x = 4
+print(x)
+
+# my name = "Yuri" # will gen an error
+
+my_name = "Yuri"
+print(my_name)
+
+# if the variable is uppercased, use uppercased, if the variable is lowercase, use lowercase
+# if the variable is uppercased and you use lowercase to reference, will get an error
+
+# the variable can't start with numbers
+
+a4 = 4 # is right
+# 4a = 4 # will get an error
+
+# exist some reserved words, like if, while, etc..
+# if you try to make a variable with reserved words, will get an error
+
+# python don't save in memory anything after the runned script, so if you want to save a data in a memory, you need to save in a archive, database, excel archive, etc..
+
