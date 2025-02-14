700dac687702e3abd0df97ad32e8302dea916a1f
yuridomingues <yuridomingues.contato@gmail.com>
2025-02-14 13:58:58 -0300

    first commit

---
+++ basic_concepts/fundamentals/helloworld.py
@@ -0,0 +1,3 @@
+print('Hello World!')
+print('My name is Yuri!')
+print('I am learning Python!')
---
+++ basic_concepts/fundamentals/numbers.py
@@ -0,0 +1,24 @@
+print (1 + 2) #3, int number
+print (3 - 5) #-2, int number
+print (10 / 4) #2.5, float number
+print (3 * 3) #9 , int number
+print (5 ** 2) #25, int number
+
+# how to check if the variable type?
+
+print(type(5.0)) # <class 'int'>
+# we can use the type method to check variable type
+
+# but this isn't a error in python, we can use this:
+
+print(5.0 + 5) # 10.0
+
+print(2 * 4 + 6) # 14
+
+# to use priority patterns we can use parenthesis, like in math
+
+print(2 *(4 + 6)) # 20
+
+# but he is limited in math patterns like division per 0, will show a error
+# decimal numbers is always "." not ","
+
