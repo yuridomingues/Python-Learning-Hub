0ed512fc2ee63931f4b617d6c7ee23c2ca7ec1f6
yuri <yuridomingues.contato@gmail.com>
2025-02-15 09:37:01 -0300

    feat: learning comparison operators

---
+++ flow_control/comparison_operators.py
@@ -0,0 +1,21 @@
+age = int(input('Write your age: '))
+
+# under
+if age < 18:
+    print('You have under 18 years')
+
+# more
+if age > 18:
+    print('You have more than 18 years')
+
+# equal
+
+if age == 18:
+    print('You have 18 years')
+
+# difference
+if age != 122:
+    print('You are not Jeanne Calment')
+
+# 4 == 4.0 is true
+# 5 == '5' is false
