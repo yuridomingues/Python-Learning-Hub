ae527c1fc197b3f46bd6d4d482435f29df7b37ae
yuri <yuridomingues.contato@gmail.com>
2025-02-15 09:31:17 -0300

    feat: add example flow control

---
+++ flow_control/example.py
@@ -0,0 +1,11 @@
+age = int(input('Insert your age: '))
+
+if age < 18:
+    print('You are under 18')
+    print('You cant drive a car')
+
+elif age == 18:
+    print('You have 18 years!')
+
+else: 
+    print('You are more than 18')
