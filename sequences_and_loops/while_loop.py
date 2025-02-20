9b376a6d03c7aefd19a5cab43c8950f40c205b59
yuri <yuridomingues.contato@gmail.com>
2025-02-20 17:40:47 -0300

    feat: add while loop

---
+++ sequences_and_loops/while_loop.py
@@ -0,0 +1,8 @@
+n = 0  # Initialize the variable n to 0
+
+# Start a while loop that will run as long as n is less than 3
+while n < 3: 
+    print(f'The value of n is: {n}')  # Print the current value of n
+    n += 1  # Increment n by 1
+
+print('The loop is over')  # Print a message indicating the loop has ended
