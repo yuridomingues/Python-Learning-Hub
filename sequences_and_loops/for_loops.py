9ba4a56beb220ef109fc84a54fa399055bff9ae9
yuridomingues <yuridomingues.contato@gmail.com>
2025-02-20 12:22:50 -0300

    feat: add for loops

---
+++ sequences_and_loops/for_loops.py
@@ -0,0 +1,11 @@
+# This loop will iterate over the numbers from 0 to 9
+for n in range(10):
+    # Print the value of n
+    print(f'The value of n is: {n}')
+    # Loop will repeat
+    # The variable n doesn't need to be defined before the loop
+    # If you don't need the variable for anything, you can use the variable "_"
+
+# This loop will repeat 5 times
+for _ in range(5):
+    print('Hello!')
