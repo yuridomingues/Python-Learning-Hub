6789bf1640c21b9225efbd9ff8c3d2d42d021b3f
yuri <yuridomingues.contato@gmail.com>
2025-02-22 10:30:45 -0300

    feat: add dictorianaries

--- dictionaries/dictionaries.py
+++ dictionaries/dictionaries.py
@@ -5,3 +5,32 @@ capitals = {'Brazil': 'Brasília',
 }
 
 print(capitals['Brazil'])
+# Will show "Brasília"
+
+# Dictionaries are mutable
+
+capitals['England'] = 'London'
+
+print(capitals['England'])
+# Will show 'London'
+
+del capitals['England']
+# Will delete
+
+for country in capitals:
+    capital = capitals[country]
+    print(f'The capital of {country} is: {capital}')
+
+
+d = dict()
+d[10] = 'abc'
+d['KEY'] = 5
+d[3.15] = True
+
+# print(d)
+
+# for k in d:
+#    v = d[k]
+#    print(f'Key: {k}, Value: {v}')
+
+print(d.items())
